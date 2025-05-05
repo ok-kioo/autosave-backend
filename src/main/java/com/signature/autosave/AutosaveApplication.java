1d6c89fa99bf6970933720ba8475c09375494a05
kio <kaio.viana@upe.br>
2025-05-05 20:56:20 -0300

    Initial commit

---
+++ src/main/java/com/signature/autosave/AutosaveApplication.java
@@ -0,0 +1,13 @@
+package com.signature.autosave;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutosaveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutosaveApplication.class, args);
+	}
+
+}
